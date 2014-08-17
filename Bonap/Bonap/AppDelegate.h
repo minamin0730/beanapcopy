a058c776ac5d0693251336639bd94e5a0089a998
岡田みなみ <minami@hit.is.kit.ac.jp>
2014-08-17 16:46:05 +0900

    xcodeprojectとgitignoreを追加

---
+++ Bonap/Bonap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bonap
+//
+//  Created by 岡田みなみ on 2014/08/17.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bonap/Bonap/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Bonap
+//
+//  Created by 岡田みなみ on 2014/08/17.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

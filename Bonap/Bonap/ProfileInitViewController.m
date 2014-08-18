0b6f108f0db5223cc9094db3f0956f33d975f28f
岡田みなみ <minami@hit.is.kit.ac.jp>
2014-08-18 16:31:22 +0900

    ストーリーボードがおおまかに出来た

---
+++ Bonap/Bonap/ProfileInitViewController.m
@@ -0,0 +1,49 @@
+//
+//  ProfileInitViewController.m
+//  Bonap
+//
+//  Created by 岡田みなみ on 2014/08/18.
+//  Copyright (c) 2014年 岡田みなみ. All rights reserved.
+//
+
+#import "ProfileInitViewController.h"
+
+@interface ProfileInitViewController ()
+
+@end
+
+@implementation ProfileInitViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

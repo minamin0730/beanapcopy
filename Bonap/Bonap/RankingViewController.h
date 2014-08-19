6e6482bba43a5fe6cffdebb2f8de7365a17d79d8
岡田みなみ <minami@hit.is.kit.ac.jp>
2014-08-19 23:35:09 +0900

    ランキングツイート（ハッシュタグ検索の取得）を統合

--- Bonap/Bonap/RankingViewController.h
+++ Bonap/Bonap/RankingViewController.h
@@ -1,13 +1,16 @@
 //
-//  RankingViewController.h
-//  Bonap
+//  ViewController.h
+//  TwitterFrameworkSample
 //
-//  Created by 岡田みなみ on 2014/08/18.
-//  Copyright (c) 2014年 岡田みなみ. All rights reserved.
+//  Created by 岡田みなみ on 2014/08/19.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
-@interface RankingViewController : UIViewController
+@interface RankingViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
+
 
 @end
